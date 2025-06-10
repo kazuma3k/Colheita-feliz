4d731e166d17d7319c0f96f9f912cc4c68b69777
kazuma3k <joao.lass.carneiro@escola.pr.gov.br>
2025-06-10 13:57:59 -0300

    a

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function telaInicial() {
   rect(0, 0, width, height);
   fill(255);
   textSize(36);
-  text("🚛 Corrida da Colheita 🐄", width / 2, height / 2 - 60);
+  text("🌹 Corrida bizarra 🦠", width / 2, height / 2 - 60);
   textSize(20);
   text("Use as setas ↑ e ↓ para mover o caminhão", width / 2, height / 2 - 10);
   text("Colete os alimentos e evite os obstáculos!", width / 2, height / 2 + 20);
@@ -85,7 +85,7 @@ function jogar() {
     alimentos.push({
       x: width + random(50, 150),
       y: random(50, height - 100),
-      emoji: random(["🍎", "🌽", "🥕"])
+      emoji: random(["🍓", "🌽", "🥕"])
     });
     timerItens = 0;
   }
@@ -107,7 +107,7 @@ function jogar() {
     if (colidiuComCaminhao(alimentos[i])) {
       pontuacao++;
       switch (alimentos[i].emoji) {
-        case "🍎": contagemMaça++; break;
+        case "🍓": contagemMorango++; break;
         case "🌽": contagemMilho++; break;
         case "🥕": contagemCenoura++; break;
       }
@@ -144,7 +144,7 @@ function jogar() {
   fill(0);
   textSize(18);
   textAlign(LEFT);
-  text("🍎: " + contagemMaça, 10, 20);
+  text("🍓: " + contagemMorango, 10, 20);
   text("🌽: " + contagemMilho, 10, 45);
   text("🥕: " + contagemCenoura, 10, 70);
   textAlign(RIGHT);
@@ -167,7 +167,7 @@ function telaGameOver() {
   text("Fim de jogo!", width / 2, height / 2 - 150);
   textSize(24);
   text("Pontuação: " + pontuacao, width / 2, height / 2 - 50);
-  text("🍎 Maçãs: " + contagemMaça, width / 2, height / 2 - 20);
+  text("🍓 Morangos: " + contagemMorango, width / 2, height / 2 - 20);
   text("🌽 Milhos: " + contagemMilho, width / 2, height / 2 + 10);
   text("🥕 Cenouras: " + contagemCenoura, width / 2, height / 2 + 40);
   text("🏆 Recorde: " + recorde, width / 2, height / 2 + 80);
